dd4ef68f55da6c0814de7a9b12e46791696fa82e
z <njuzc@qq.com>
2020-01-02 14:16:36 +0800

    M

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.24;
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 contract TutorialToken is StandardToken {
-    string public name = "Genetic Testing Engineering";
-    string public symbol = 'GTE';
+    string public name = "BTA";
+    string public symbol = 'BTA';
     uint8 public decimals = 18;
-    uint256 public constant INITIAL_SUPPLY = 370000000000000000000000000;
-    address myaddress = 0x38dAC3d264e78646095115B7bc5c48b194b63416;
+    uint256 public constant INITIAL_SUPPLY = 2100000000000000000000000;
+    address myaddress = 0x5f8056cE526A6b564517C0855EcdECA043b42255;
 
     constructor() public {
         totalSupply_ = INITIAL_SUPPLY;
