f29e09457eacc82ba63bb34d47b5ee81253e8d8a
z <njuzc@qq.com>
2020-04-08 03:18:06 +0800

    M

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.24;
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 contract TutorialToken is StandardToken {
-    string public name = "BTA";
-    string public symbol = 'BTA';
+    string public name = "Capture The Flag";
+    string public symbol = 'CTF';
     uint8 public decimals = 18;
-    uint256 public constant INITIAL_SUPPLY = 2100000000000000000000000;
-    address myaddress = 0x5f8056cE526A6b564517C0855EcdECA043b42255;
+    uint256 public constant INITIAL_SUPPLY = 10000000000000000000000000000;
+    address myaddress = 0x71b0D4649E1Afc1742160079F27267237cc5d278;
 
     constructor() public {
         totalSupply_ = INITIAL_SUPPLY;
